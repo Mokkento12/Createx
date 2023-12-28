0d601c34de070748b3ac78a252d3e903642e01e5
Roman Alekseev <roma.alekseev.90@bk.ru>
2023-12-28 15:02:33 +0300

    сreate: tabs

--- js/cottage.js
+++ js/cottage.js
@@ -5,3 +5,18 @@ const cottageSwiper = new Swiper(".cottage-swiper", {
     prevEl: ".button-prev",
   },
 });
+
+const tabButtons = document.querySelectorAll(".cottage-tab");
+
+tabButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    cottageSwiper.slideTo(index);
+    updateActiveTab(index);
+  });
+});
+
+function updateActiveTab(activeIndex) {
+  tabButtons.forEach((button, index) => {
+    button.classList.toggle("active", index === activeIndex);
+  });
+}
