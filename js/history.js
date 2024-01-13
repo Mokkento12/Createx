6cb53f4052d6a268367619d0fa80a8b679c300a3
Roman Alekseev <roma.alekseev.90@bk.ru>
2024-01-13 21:26:29 +0300

    create: add tabs fix

--- js/history.js
+++ js/history.js
@@ -1,10 +1,35 @@
-const historySwiper = new Swiper(".history-swiper", {
-  // Optional parameters
-  loop: true,
+// const historySwiper = new Swiper(".history-swiper", {
+//   // Optional parameters
+//   loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".history-button-next",
-    prevEl: ".history-button-prev",
-  },
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".history-button-next",
+//     prevEl: ".history-button-prev",
+//   },
+// });
+document.addEventListener("DOMContentLoaded", () => {
+  const historySwiper = new Swiper(".history-swiper", {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".history-button-next",
+      prevEl: ".history-button-prev",
+    },
+    on: {
+      slideChange: function (swiper) {
+        var activeIndex = swiper.realIndex; // Поправлено на swiper.realIndex
+        setActiveTab(activeIndex);
+      },
+    },
+  });
+
+  function setActiveTab(index) {
+    var dates = document.querySelectorAll(".date");
+    dates.forEach(function (date, i) {
+      date.classList.toggle("active", i === index);
+    });
+  }
 });
