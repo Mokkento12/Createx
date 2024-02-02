07f2f276cafb199bade01a5a19d1a195ef6e548f
Roman Alekseev <roma.alekseev.90@bk.ru>
2024-02-02 22:37:52 +0300

    create: pagination

--- js/news.js
+++ js/news.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   const tabs = document.querySelectorAll(".news-tab");
   const swiperContainer = document.querySelector(".swiper-container");
+  const nums = document.querySelectorAll(".num");
+  const prevBtn = document.querySelector(".num-btn__prev");
+  const nextBtn = document.querySelector(".num-btn__next");
 
   const mySwiper = new Swiper(swiperContainer, {
     loop: true,
@@ -16,5 +19,36 @@ document.addEventListener("DOMContentLoaded", function () {
       tab.classList.add("active");
       mySwiper.slideTo(index);
     });
+  });
+
+  nums.forEach(function (num) {
+    num.addEventListener("click", function () {
+      nums.forEach(function (el) {
+        el.classList.remove("active");
+      });
+      num.classList.add("active");
+    });
+  });
+
+  prevBtn.addEventListener("click", function () {
+    const currentActive = document.querySelector(".num.active");
+    if (
+      currentActive.previousElementSibling &&
+      currentActive.previousElementSibling.classList.contains("num")
+    ) {
+      currentActive.classList.remove("active");
+      currentActive.previousElementSibling.classList.add("active");
+    }
+  });
+
+  nextBtn.addEventListener("click", function () {
+    const currentActive = document.querySelector(".num.active");
+    if (
+      currentActive.nextElementSibling &&
+      currentActive.nextElementSibling.classList.contains("num")
+    ) {
+      currentActive.classList.remove("active");
+      currentActive.nextElementSibling.classList.add("active");
+    }
   });
 });
